7664384bd9cf295c925307ba6f01eb3ca94f525d
macbook <keskin.atakansak@gmail.com>
2021-11-06 15:15:23 +0300

    FriendListViewController UI has been designed and implemented with programmatic collectionView, Custom CollectionViewCell

---
+++ FacebookFriends/Views/FriendCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FriendCollectionViewCell.swift
+//  FacebookFriends
+//
+//  Created by macbook on 5.11.2021.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    
+}
